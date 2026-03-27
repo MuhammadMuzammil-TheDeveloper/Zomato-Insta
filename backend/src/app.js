131bb19aae7e04e6f5661d181d15835400c3bed0
MuhammadMuzammil-TheDeveloper <muzammil.thedeveloper@gmail.com>
2026-03-27 12:46:26 +0500

    buil authentication API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,14 @@
 //  create Server
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
+
+
 const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Welcome to  Insta");
